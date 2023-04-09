c7aa595f4248d2c80cb783fd10b765931b93312c
dyllisLev <dyllis.lev@gmail.com>
2023-04-09 21:05:18 +0900

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Export environment variables
+export RUNNING_TYPE=docker
+export C_FORCE_ROOT=true
+
+# Start Redis server in daemon mode
+redis-server --daemonize yes
+
+# Initialize counter variable
+COUNT=0
+
+# Start FlaskFarm and repeat if necessary
+while true;
+do
+    # Upgrade FlaskFarm package
+    pip install --upgrade FlaskFarm
+
+    # Run FlaskFarm with specified configuration
+    python -m flaskfarm.main --repeat ${COUNT} --config "~/ff/data/config.yaml"
+
+    # Store exit code
+    RESULT=$?
+
+    # Print exit code
+    echo "PYTHON EXIT CODE : ${RESULT}.............."
+
+    # Check exit code and repeat if necessary
+    if [ "$RESULT" = "1" ]; then
+        echo 'REPEAT....'
+    else
+        echo 'FINISH....'
+        break
+    fi
+
+    # Increment counter variable
+    COUNT=`expr $COUNT + 1`
+done
