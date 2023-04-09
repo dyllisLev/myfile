f2130a0ff993db8a7c2c990a5df0e6e74ff64372
dyllisLev <dyllis.lev@gmail.com>
2023-04-09 21:21:50 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 pip install flaskfarm
 find / -xdev -type d -name "flaskfarm" -exec sh -c 'pip install -r "$0/files/requirements_normal.txt"' {} \;
 
-mkdir -p ~/ff && cd ~/ff && curl -O https://raw.githubusercontent.com/dyllisLev/myfile/main/run.sh && chmod +x run.sh
+mkdir -p /home/codespace/ff && cd /home/codespace/ff && curl -O https://raw.githubusercontent.com/dyllisLev/myfile/main/run.sh && chmod +x run.sh
 
-mkdir -p ~/ff/data
+mkdir -p /home/codespace/ff/data
  
-echo "path_data: /home/codespace/ff/data" > ~/ff/data/config.yaml
-echo "path_dev: /workspaces" >> ~/ff/data/config.yaml
-echo "debug: true" >> ~/ff/data/config.yaml
+echo "path_data: /home/codespace/ff/data" > /home/codespace/ff/data/config.yaml
+echo "path_dev: /workspaces" >> /home/codespace/ff/data/config.yaml
+echo "debug: true" >> /home/codespace/ff/data/config.yaml
