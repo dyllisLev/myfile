b82ba3da35d3b61af0d1a5b3b6827378b9c90cc3
dyllisLev <dyllis.lev@gmail.com>
2023-04-09 21:01:24 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-mkdir -p ~/ff && cd ~/ff && curl -O https://cdn.discordapp.com/attachments/621288921493667872/1094591935613784074/install.sh && chmod +x install.sh & ~/ff/install.sh
+pip install flaskfarm
+find / -xdev -type d -name "flaskfarm" -exec sh -c 'pip install -r "$0/files/requirements_normal.txt"' {} \;
+
+mkdir -p ~/ff && cd ~/ff && curl -O https://cdn.discordapp.com/attachments/621288921493667872/1094588952071131166/run.sh && chmod +x run.sh
+
+mkdir -p ~/ff/data
+ 
+echo "path_data: ~/ff/data" > ~/ff/data/config.yaml
+echo "path_dev: /workspaces" >> ~/ff/data/config.yaml
+echo "debug: true" >> ~/ff/data/config.yaml
